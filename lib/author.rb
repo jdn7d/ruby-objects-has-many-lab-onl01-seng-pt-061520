2abccebf525fbe7f2f5214539691f89ff8567c9c
Jessie Donovan <donovanajessie@gmail.com>
2020-07-18 05:15:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,30 @@
 class Author
   attr_accessor :name
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def add_post
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    add_post(post)
-    end
-
-    def posts
-    Post.all.select {|post| post.author == self}
+    post.author = self
   end
 
-    def self.post_count
-      Post.all.count
-    end
+  def posts
+    Post.all.select {|p| p.author == self}
+  end
 
-
+  def self.post_count
+    Post.all.count
+  end
 end
